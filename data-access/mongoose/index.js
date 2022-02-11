80023b83ed17893c5ac771466a5218287e9c2c6a
Teo Toledo <teootoledo@gmail.com>
2022-02-10 21:47:26 -0300

    fix(dals): Agrego try/catch en dals

--- data-access/mongoose/index.js
+++ data-access/mongoose/index.js
@@ -9,31 +9,39 @@ const persistStore = async (
   active,
   lastSale
 ) => {
-  const newStore = store.create({
-    name,
-    cuit,
-    concepts,
-    currentBalance,
-    active,
-    lastSale,
-  });
+  try {
+    const newStore = store.create({
+      name,
+      cuit,
+      concepts,
+      currentBalance,
+      active,
+      lastSale,
+    });
 
-  return newStore;
+    return newStore;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const obtainStores = async (page, limit) => {
-  const totalStores = await store.countDocuments();
-  const docsToSkip = (page - 1) * limit;
-  const stores = await store.find().skip(docsToSkip).limit(limit);
-  const storesWithPagination = {
-    data: formatStores(stores),
-    page,
-    pages: Math.ceil(totalStores / limit),
-    limit: limit,
-    total: totalStores,
-  };
+  try {
+    const totalStores = await store.countDocuments();
+    const docsToSkip = (page - 1) * limit;
+    const stores = await store.find().skip(docsToSkip).limit(limit);
+    const storesWithPagination = {
+      data: formatStores(stores),
+      page,
+      pages: Math.ceil(totalStores / limit),
+      limit: limit,
+      total: totalStores,
+    };
 
-  return storesWithPagination;
+    return storesWithPagination;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
